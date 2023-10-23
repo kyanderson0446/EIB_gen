32ca190dc16015764e265fe1d0c05863635370af
Ky.Anderson46 <andersonkyle099@gmail.com>
2023-10-23 10:12:51 -0600

    Update EIB_gen.py
    This is to generate the current year only and update some variables.

--- EIB_gen.py
+++ EIB_gen.py
@@ -7,43 +7,61 @@ import sys
 import time
 import win32com.client
 import datetime
+import os
 
-app = xw.App(add_book=False)
-xlwings.App.display_alerts = False
+# app = xw.App(add_book=False)
+# xlwings.App.display_alerts = False
 
 # Accessing Files and Pdrive
 print("Access Folder")
 folder = str(input("Enter \"YYYY Quarter #\": "))
+quarter_entry = folder.split(" ")
+quarter_entry = quarter_entry[1]
+
+year_entry = folder.split(" ")
+year_entry = year_entry[0]
+year_entry = int(year_entry)
+
+# get the year and once each year forecast is complete, loop through current year+x
+
 eib_max = 65
 print("*" * 60)
 
-path = fr"P:\PACS\Finance\Budgets\{folder}\Received\Final Q1\*.xlsx"
-EIB_wb = xw.Book(fr"C:\Users\kyle.anderson\Documents\Courtney\Courtney PACS_Import_Budget-test.xlsm")
+# path = fr"P:\PACS\Finance\Budgets\{folder}\Received\Final Q1\*.xlsx"
+df_fac = pd.read_csv("Facility_list.csv")
+df_fac = df_fac['Facility']
+facilities = list(df_fac)
+
+
+EIB_wb = xw.Book(fr"C:\Users\kyle.anderson\OneDrive - PACS\backup\Documents\Courtney PACS_Import_Budget-test-main 5 year - Copy.xlsm")
+
+start_date_key = {'Q1': f'1/1/{year_entry}', 'Q2': f'4/1/{year_entry}',
+                  'Q3': f'7/1/{year_entry}', 'Q4': f'10/1/{year_entry}'}
 
 # Begin For Loop of Opening Budgets and Pasting Values Into EIB
 x = 1
+current_year = 0
 
-for file in glob(path):
-    budget_wb = xw.Book(file, update_links=False)
-    upload_page = budget_wb.sheets("DW Upload")
-    facility_info = budget_wb.sheets("FACILITY INFO")
 
-    upload_name = facility_info.range("B7").value
-    print(upload_name)
-    budget_data = upload_page.range("A1:AB374")
-
-    EIB_wb.sheets["WD Upload"].range(f"A1:AB374").value = budget_data.value
-
-# Run Macros
+for facility in facilities:
+    print(fr"Creating file for {quarter_entry} {facility}")
+    EIB_wb.sheets["WD Upload"].range(f"A1").value = facility
+    current_quarter_date = fr"1/1/{year_entry}"
+    EIB_wb.sheets["WD Upload"].range(f"C2").value = current_quarter_date
+    time.sleep(1)
+    # year start date
+    EIB_wb.sheets["WD Upload"].range(f"C3").value = year_entry
+    time.sleep(9)
+    # Run Macros
     macro2 = EIB_wb.macro("Module2.Clear_all")
     macro2()
-    time.sleep(1)
-    macro1 = EIB_wb.macro("Module1.GL_reformat")
+    time.sleep(3)
+    macro1 = EIB_wb.macro("Module1.Run_refresh")
     macro1()
-    time.sleep(1)
+    time.sleep(10)
     macro1 = EIB_wb.macro("Module1.GL_code")
     macro1()
-    time.sleep(1)
+    time.sleep(6)
     macro1 = EIB_wb.macro("Module1.Credit_amount")
     macro1()
     time.sleep(1)
@@ -52,13 +70,15 @@ for file in glob(path):
     time.sleep(1)
     macro1 = EIB_wb.macro("Module1.Month_drop")
     macro1()
-    time.sleep(2)
+    time.sleep(4)
 
-    EIB_wb.save(fr"C:\Users\kyle.anderson\Documents\Courtney\{upload_name} EIB_budget.xlsx")
-    budget_wb.close()
-
-    time.sleep(1)
-
+    EIB_wb.save(
+        fr"C:\Users\kyle.anderson\OneDrive - PACS\backup\Documents\Courtney\{quarter_entry} {facility} EIB_budget.xlsx")  # change quarter
+# EIB_wb.close()
+# app.quit()
+current_year += 1
+year_entry += 1
+time.sleep(1)
 
 print()
 print(time.process_time(), "minutes")
