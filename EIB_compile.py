42e2e1e28857c4b997c0a2ad94410a53b2107067
Ky.Anderson46 <andersonkyle099@gmail.com>
2023-10-23 10:15:45 -0600

    Update EIB_compile.py
    general update of new code

--- EIB_compile.py
+++ EIB_compile.py
@@ -10,25 +10,26 @@ import datetime
 
 app = xw.App(add_book=False)
 xlwings.App.display_alerts = False
-main_eib = xw.Book(fr"C:\Users\kyle.anderson\Documents\WD_upload_budget_test.xlsx")
-path = fr"C:\Users\kyle.anderson\Documents\Courtney\*.xlsx"
-y = 1
+main_eib = xw.Book(fr"C:\Users\kyle.anderson\OneDrive - PACS\backup\Documents\WD_upload_budget_main.xlsx")
+path = fr"C:\Users\kyle.anderson\OneDrive - PACS\backup\Documents\Courtney\2028\*.xlsx"
 x = 6
+
+
 for file in glob(path):
     budget_wb = xw.Book(file, update_links=False)
     upload_page = budget_wb.sheets("Budget Lines Data")
 
-    upload_data = upload_page.range("B6:M1493")
+    upload_data = upload_page.range("B6:M1241")
     time.sleep(1)
 
     main_eib.sheets["Budget Lines Data"].range(f"B{x}:M{x}").value = upload_data.value
     time.sleep(1)
 
-    x += 1488
-    y += 1
-    print(y)
-    budget_wb.close()
+    x += 1236
 
-main_eib.save(fr"C:\Users\kyle.anderson\Documents\WD_upload_budget_test.xlsx")
+    budget_wb.close()
+print("STOP \n " * 3 )
+
+main_eib.save(fr"C:\Users\kyle.anderson\OneDrive - PACS\backup\Documents\Courtney\WD_upload_budget_batch_Q1-28.xlsx")
 
 
